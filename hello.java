39ea7dd4f24084cc5dcf9f4d1eaf6a7b3be799e5
Mohit <mohitwadhwani210@gmail.com>
2018-07-27 12:07:36 +0530

    java file commited

---
+++ hello.java
@@ -0,0 +1,15 @@
+import java.util.Scanner;
+class num
+{
+	public static void main(String [] args)
+	{
+		int a,b,n;
+		Scanner in=new Scanner(System.in);
+		System.out.println("enter two numbers");
+		a=in.nextInt();
+		b=in.nextInt();
+		System.out.println("enter nth number which is divisble by one of the above and both number");
+		n=in.nextInt();
+          System.out.println((n/a)*(n/b));
+		}
+	}[B[B[B[B[B[B[B[B[B[B
